efc6fe35818d6d9947fef06186eb8113e861ff65
Jeremy Durdel <jeremydurdeljr@gmail.com>
2025-07-02 10:41:11 -0400

    Assignment Complete

--- Chapter 9 and 10 Lab 1.py
+++ Chapter 9 and 10 Lab 1.py
@@ -2,10 +2,12 @@
 #Chapter 9 and 10 Lab 1
 #07/02/2025
 
+#Main Function, takes information and combines it all
 def main():
     print("Name Processor")
     print()
     full_name = get_full_name()
+    #Will fix capitalization in the form of a title(first letter upper case, all else lower)
     full_name = full_name.title()
 
     first_name, last_name = split_name(full_name)
@@ -15,6 +17,7 @@ def main():
 def get_full_name():
     while True:
         name = input("Enter your name: ").strip()
+        #This checks for a space or a comma to make sure it's a full name
         if " " in name or "," in name:
             return name
         else:
@@ -23,10 +26,12 @@ def get_full_name():
 
 def split_name(name):
     if "," in name:
+        #Formats based on how the input is formatted
         index = name.split(",")
         first = index[0].strip()
         last = index[1].strip()
     else:
+        #Formats based on how the input is formatted
         index = name.split()
         last = index[0]
         first = index[1]
